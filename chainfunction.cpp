1150b01a1e8fefd97020f58811c1ade1c6bb8b05
BilkisShakila <bilkisshakilaa@gmail.com>
2025-05-17 15:26:49 +0700

    main function

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -1,2 +1,29 @@
 #include <iostream>
 using namespace std;
+
+class buku
+{
+    string judul;
+
+public:
+    buku setjudul(string judul)
+    {
+        this->judul = judul;
+        return *this; // chain funtion
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
+
+int main()
+{
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setjudul("Matematika").getJudul(); // chain funtion calls
+
+    return 0;
+    
+    
+}
