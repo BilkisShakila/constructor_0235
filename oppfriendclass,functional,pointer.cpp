aee67ad6ea3328aee4caaa439ff7eacd75687b1c
BilkisShakila <bilkisshakilaa@gmail.com>
2025-05-17 16:04:46 +0700

    kelas mahasiswa

--- oppfriendclass,functional,pointer.cpp
+++ oppfriendclass,functional,pointer.cpp
@@ -3,5 +3,24 @@
 using namespace std;
 
 // Deklarasi awal agar bisa digunakan sebagai friend
-class Dosen;
-class Staff;
+    class Dosen;
+    class Staff;
+
+class Mahasiswa {
+private:
+    string nama;
+    int nim;
+    float nilai;
+
+public:
+    Mahasiswa(string pNama, int pNim) : nama(pNama), nim(pNim), nilai(0.0f) {}
+
+    void tampilkanInfo() {
+        cout << "Nama Mahasiswa : " << nama << endl;
+        cout << "NIM            : " << nim << endl;
+        cout << "Nilai          : " << nilai << endl;
+    }
+
+    // Dosen dapat mengakses dan mengubah nilai karena friend class
+    friend class Dosen;
+};
